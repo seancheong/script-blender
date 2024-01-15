8a0b05cdf2c2eac68512d3ec71fb5f3d5d13bc5f
Sean Cheong Zhen Xiong <sean21cheong@msn.com>
2024-01-15 22:25:14 +0900

    Fix warning from logo image

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -5,9 +5,10 @@ export const Header = () => {
     <header className='flex gap-2 h-12 px-4 py-2'>
       <Image
         src='./logo.svg'
-        width={42}
-        height={42}
+        width={0}
+        height={0}
         alt='script-blender-logo'
+        style={{ width: '42px', height: 'auto' }}
       />
 
       <h1 className='text-2xl font-semibold text-zinc-50'>Script Blender</h1>
